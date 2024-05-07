6940962e577fe9b8278d1341e1663047ea6251f5
dh-pardeep <pardeepjangra514@gmil.com>
2024-05-07 12:20:42 +0530

    footer done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,7 @@
-// text
+// ================ YEAR FUNCTION
+// JavaScript function to get current year
+function getCurrentYear() {
+  return new Date().getFullYear();
+}
+// Update the content of the span element with the current year
+document.getElementById("currentYear").innerText = getCurrentYear();
