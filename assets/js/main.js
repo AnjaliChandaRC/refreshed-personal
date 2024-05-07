f1e22035ceda3e3939f14a817f3a6c6af6927501
dh-pardeep <pardeepjangra514@gmil.com>
2024-05-07 15:08:21 +0530

    css merge

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,45 @@ function getCurrentYear() {
 // Update the content of the span element with the current year
 document.getElementById("currentYear").innerText = getCurrentYear();
 
+// ===================== AUTHOR SLIDER JS
+$(".author_slider").slick({
+  arrows: false,
+  dots: false,
+  speed: 500,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2.5,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        dots: true,
+      },
+    },
+  ],
+});
+
+
+
+
+
+
+
+
+
